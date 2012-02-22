f6f99571d37552e144b2f09f84ec69a31d66a602
tdaniel16@gmail.com <tdaniel16@gmail.com@0cdcdda5-9da0-63a2-563e-abc94933a68b>
2012-02-22 20:46:14 +0000

    Updated:  New analyses, new sessions, a new mapper, and a new splitter.
    MTSOFSAnalysis:
    	This analysis package is used for the Matching-to-Sample / Oddity-from-Sample reversal program.  The first 48 trials are treated as MTS, and the last 48 trials are treated as OFS.  Data is usually analyzed in bins of 12 trials.  An accompanying MTSOFSSession builder is also included.
    	
    OFSAnalysis:
    	This analysis package is used for the Oddity-from-Sample program.  This code includes all set-size expansion stimuli; this can be used in conjunction with the ItemSplitter splitter included.  An accompanying OFSSession builder is also included.
    	
    MoveAnalysis:
    	This analysis package is used for the Oddity-from-Sample and Stimulus Movement programs.  Functionally, this package is very similar to OFSAnalysis.  An accompanying MovementSession builder is also included.
    	
    PseudoSDAnalysis:
    	The analysis package is used for the Pseudoconceptual Learning program for pigeons.  This code includes all set-size expansion stimuli for set-sizes 8, 16, 32, and 64.  
    	
    SamplePecksAverageMap:
    	This mapper records the mean number of observations a sample stimulus receives in a session.
    	
    ItemSplitter:
    	This splitter reports the mean performance on trials based on their sample and comparison items' learning history.  Creating a vector in the appropriate analysis program, individual image names can be added to assign "novel" or "familiar" labels to configurations in the session.

--- Katalabaino/src/analyses/PseudoSDAnalysis.java
+++ Katalabaino/src/analyses/PseudoSDAnalysis.java
@@ -6,62 +6,81 @@ import java.util.Vector;
 import mappers.AccuracyMap;
 import mappers.SessionInfoMap;
 import sessions.PseudoSDSession;
-import splitters.PseudoTrueSplitter;
-import testing.ItemPairAccuracyMap;
-import testing.MultiSessionHDAnalysis;
+import splitters.ItemSplitter;
 import core.Analysis;
 import core.FileTypeConverter;
 import core.Session;
 import core.SessionFactory;
-import filters.ComparisonRule;
-import filters.SessionIDFilter;
 
-public class PseudoSDAnalysis
-{
-	public static void main(String[] args) throws Exception
-	{
-		String dir = "W:/warehouse/pseudo/acquisition/";
-		// String dir = "/Users/jmagnotti/warehouse/pseudo/8_acquisition/";
-		String bird = "shredder";
+@SuppressWarnings("unused")
+public class PseudoSDAnalysis {
+	public static void main(String[] args) throws Exception {
+		String dir = "C:/information/pseudo/32acquisition/";
+		String bird = "rocksteady";
 		String workDir = dir + bird + "/";
 
-		 FileTypeConverter.CreateZipFileFromDirectory(workDir, bird, new PseudoSDSession());
+		// call this once each time you add new results files
+		FileTypeConverter.CreateZipFileFromDirectory(workDir, bird,
+				new PseudoSDSession());
+
 		File zipFile = new File(workDir + bird + ".dbo");
 
-		Vector<Session> sessions = SessionFactory.BuildSessions(new PseudoSDSession(), zipFile);
+		Vector<Session> sessions = SessionFactory.BuildSessions(
+				new PseudoSDSession(), zipFile);
 
 		Analysis analysis = new Analysis(sessions);
+
+		 Vector<String> includedImages = new Vector<String>();
+		// original 8-item set
+		 includedImages.add("T310.tga");
+		 includedImages.add("T301.tga");
+		 includedImages.add("T197.tga");
+		 includedImages.add("T244.tga");
+		 includedImages.add("T135.tga");
+		 includedImages.add("T415.tga");
+		 includedImages.add("T13.tga");
+		 includedImages.add("T76.tga");
+		// items learned in 16-item set
+		// includedImages.add("T114.tga");
+		// includedImages.add("T119.tga");
+		// includedImages.add("T384.tga");
+		// includedImages.add("T423.tga");
+		// includedImages.add("T186.tga");
+		// includedImages.add("T61.tga");
+		// includedImages.add("T56.tga");
+		// includedImages.add("T342.tga");
+		// items learned in 32-item set
+		// includedImages.add("t261.tga");
+		// includedImages.add("t42.tga");
+		// includedImages.add("t28.tga");
+		// includedImages.add("t324.tga");
+		// includedImages.add("t5.tga");
+		// includedImages.add("t205.tga");
+		// includedImages.add("t354.tga");
+		// includedImages.add("t369.tga");
+		// includedImages.add("t267.tga");
+		// includedImages.add("t411.tga");
+		// includedImages.add("t356.tga");
+		// includedImages.add("t159.tga");
+		// includedImages.add("t403.tga");
+		// includedImages.add("t26.tga");
+		// includedImages.add("t300.tga");
+		// includedImages.add("t264.tga");
+		// items learned in 64-item set
+
 		analysis.addMap(new SessionInfoMap());
+		// analysis.addMap(new AverageRTMap());
+		// analysis.addMap(new CPInfoMap());
 		analysis.addMap(new AccuracyMap());
-		analysis.addSplitter(new PseudoTrueSplitter());
-
-		// analysis.analyze();
-
-		HDAnalysis(workDir, bird);
-	}
-
-	public static void HDAnalysis(String workDir, String bird) throws Exception
-	{
-		int blockSize = 10;
-		int block = 2;
-		// for (int block = 0; block < 48 / blockSize; block++) {
-		File zipFile = new File(workDir + bird + ".dbo");
-		Vector<Session> sessions = SessionFactory.BuildSessions(new PseudoSDSession(), zipFile);
-
-		MultiSessionHDAnalysis analysis = new MultiSessionHDAnalysis(sessions);
-
-		analysis.addFilter(new SessionIDFilter(new ComparisonRule(ComparisonRule.INCLUSIVE, 
-//				(block) * blockSize + 1
-				1
-				, (block + 1) * blockSize))
-		);
-
-		// analysis.addFilter(new TrialConfigurationFilter(new ComparisonRule(
-		// ComparisonRule.INCLUSIVE, (block * blockSize) + 1, (block + 1) * blockSize)));
-
-		analysis.addMap(new ItemPairAccuracyMap());
-
+		analysis.addSplitter(new ItemSplitter(includedImages));
+		// analysis.addSplitter(new TrialTypeSplitter());
+//		analysis.addSplitter(new PseudoTrueSplitter());
+		// analysis.addSplitter(new BaselineTransferSplitter());
+		// analysis.addFilter(new CorrectTrialsOnlyFilter());
+		// analysis.addFilter(new ReactionTimeFilter(new
+		// ComparisonRule(ComparisonRule.LT_OR_EQ, 6000)));
+		// analysis.addMap(new CountMap());
+		// execute.
 		analysis.analyze();
-		// }
 	}
 }
