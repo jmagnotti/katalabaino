abcdd189fd00e857d37bc4f8fbc5f569216adcdc
john.magnotti <john.magnotti@0cdcdda5-9da0-63a2-563e-abc94933a68b>
2012-03-28 15:30:42 +0000

    quick and easy method for printing out all images used within a set of sessions. Uses the MultiSessionHDAnalysis

---
+++ Katalabaino/src/testing/ImageMap.java
@@ -0,0 +1,48 @@
+package testing;
+
+import java.util.HashMap;
+import java.util.Vector;
+
+import mappers.Mapper;
+import core.Session;
+import core.Stimulus;
+import core.Trial;
+
+public class ImageMap extends Mapper
+{
+
+	HashMap<String, String>	images;
+
+	public ImageMap()
+	{
+		super("images");
+
+		images = new HashMap<String, String>();
+	}
+
+	@Override
+	public void nextSession(Session session)
+	{
+
+	}
+
+	@Override
+	public void nextTrial(Trial trial)
+	{
+		for (Stimulus s : trial.sampleStimuli)
+			images.put(s.file, s.file);
+		for (Stimulus s : trial.choiceStimuli)
+			images.put(s.file, s.file);
+	}
+
+	@Override
+	public Vector<String> cleanUp()
+	{
+		resultString = new Vector<String>();
+
+		resultString.addAll(images.keySet());
+
+		return resultString;
+	}
+
+}
